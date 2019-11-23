bca8d71fb25af151786ee99bfa56bb6a50fcc15f
Daria Efimova <dashaefimova02@yandex.ru>
2019-11-23 12:06:54 +0300

    add models

---
+++ internal/app/admin/repository.go
@@ -0,0 +1,5 @@
+package admin
+
+type Repository struct {
+
+}
---
+++ internal/app/admin/repository/adminrep.go
@@ -0,0 +1,27 @@
+package adminrepository
+
+import "database/sql"
+
+type AdminRepository struct {
+	db *sql.DB
+}
+
+
+func NewAdminRepository(db *sql.DB) admin.Repository {
+	return &AdminRepository{db}
+}
+
+func (r *AdminRepository) Create(company *model.Company) error {
+	return r.db.QueryRow(
+		"INSERT INTO companies (companyName, site, tagLine, description, country, city, address, phone) " +
+			"VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id",
+		company.CompanyName,
+		company.Site,
+		company.TagLine,
+		company.Description,
+		company.Country,
+		company.City,
+		company.Address,
+		company.Phone,
+	).Scan(&company.ID)
+}
